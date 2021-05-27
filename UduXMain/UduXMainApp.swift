a1f66d78cc9e488dc8cac764751c73139dcfb3a1
Andy <andyeshiet@gmail.com>
2021-05-27 16:05:41 +0100

    Initial Commit

---
+++ UduXMain/UduXMainApp.swift
@@ -0,0 +1,17 @@
+//
+//  UduXMainApp.swift
+//  UduXMain
+//
+//  Created by Android Developer on 27/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct UduXMainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
